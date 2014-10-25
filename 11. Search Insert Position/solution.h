99fc44eb375dbb4da0d0b666c5293ad285ebf76c
pezy <urbancpz@gmail.com>
2014-10-25 09:20:29 +0800

    a little fix

--- 11. Search Insert Position/solution.h
+++ 11. Search Insert Position/solution.h
@@ -7,7 +7,7 @@ public:
             int mid = (low + high)/2;
             if (A[mid] == target) return mid;
             else if (A[mid] > target) high = mid - 1;
-            else if (A[mid] < target) low = mid + 1;
+            else low = mid + 1;
         }
         return low;
     }
