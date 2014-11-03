7f8aa9c1c7cb9099301dcce655d4f1fc12231de5
pezy <urbancpz@gmail.com>
2014-11-03 12:56:37 +0800

    fixed a bug : we shouldn't demolish the origin array.

--- 20. Remove Element/solution.h
+++ 20. Remove Element/solution.h
@@ -1,10 +1,12 @@
+#include <algorithm>
+
 class Solution {
 public:
     int removeElement(int A[], int n, int elem)
     {
         for (int i = 0; i < n;)
         {
-            if (A[i] == elem) A[i] = A[--n];
+            if (A[i] == elem) std::swap(A[i], A[--n]);
             else ++i;
         }
         return n;
