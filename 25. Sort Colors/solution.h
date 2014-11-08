51b14f4980078f20d910658742755957c6896f02
pezy_mbp <urbancpz@gmail.com>
2014-11-08 12:10:06 +0800

    reduce more codes.

--- 25. Sort Colors/solution.h
+++ 25. Sort Colors/solution.h
@@ -3,14 +3,8 @@
 class Solution {
 public:
     void sortColors(int A[], int n) {
-        int pos = 0;
-        for (int i=0; i != n;)
-            if (A[pos]==0) i = ++pos;
-            else if (A[i]==0) std::swap(A[pos], A[i]);
-            else ++i;
-        for (int i=pos; i != n; )
-            if (A[pos]==1) i = ++pos;
-            else if (A[i]==1) std::swap(A[pos], A[i]);
-            else ++i;
+        for (int i=0, j=0; i<n; ++i)
+            if (A[i] == 0) std::swap(A[i], A[j++]);
+            else if (A[i] == 2) std::swap(A[i--], A[--n]);
     }
 };
