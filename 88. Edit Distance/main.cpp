409a128e79df287dbcd9a8c4275f36fdd4caf69d
pezy_mbp <urbancpz@gmail.com>
2015-01-12 07:16:24 +0800

    added 88

---
+++ 88. Edit Distance/main.cpp
@@ -0,0 +1,8 @@
+#include "solution.h"
+#include <iostream>
+
+int main()
+{
+    Solution s;
+    std::cout << s.minDistance("intention", "execution") << std::endl;
+}
---
+++ 88. Edit Distance/solution.h
@@ -0,0 +1,27 @@
+#include <vector>
+using std::vector;
+#include <string>
+using std::string;
+#include <algorithm>
+using std::min;
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        if (n == 0) return m;
+        if (m == 0) return n;
+        
+        vector<vector<int>> A(n+1, vector<int>(m+1));
+        for (int i=0; i<=m; ++i) A[0][i] = i;
+        for (int i=0; i<=n; ++i) A[i][0] = i;
+        
+        for (int i=1; i<=n; ++i)
+            for (int j=1; j<=m; ++j) {
+                int cost = word1[i-1] == word2[j-1] ? 0 : 1;
+                A[i][j] = min(min(A[i-1][j]+1, A[i][j-1]+1), A[i-1][j-1]+cost);
+            }
+        return A[n][m];
+    }
+};
