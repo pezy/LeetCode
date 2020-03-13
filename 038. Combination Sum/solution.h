92e1e028894872e75a8418087da2a77160f5c42d
Neeraj jaiswal <40974623+jaiskid@users.noreply.github.com>
2020-03-13 13:39:22 +0530

    Update solution.h

--- 038. Combination Sum/solution.h
+++ 038. Combination Sum/solution.h
@@ -4,12 +4,6 @@ using std::vector;
 
 class Solution {
 public:
-    vector<vector<int> > combinationSum(vector<int> &candidates, int target) {
-        vector<vector<int>> ret;
-        std::sort(candidates.begin(), candidates.end());
-        dfs(candidates, target, vector<int>{}, 0, ret);
-        return ret;   
-    }
     void dfs(const vector<int> &cdds, int target, vector<int> curr, size_t index, vector<vector<int>> &ret) {
         if (!target) {ret.push_back(curr); return; }
         for (auto i=index; i<cdds.size(); ++i)
@@ -19,4 +13,10 @@ public:
                 curr.pop_back();
             } else break;
     }
+    vector<vector<int> > combinationSum(vector<int> &candidates, int target) {
+        vector<vector<int>> ret;
+        std::sort(candidates.begin(), candidates.end());
+        dfs(candidates, target, vector<int>{}, 0, ret);
+        return ret;   
+    } 
 };
